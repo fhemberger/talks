d7e2fb9f88db28c08ba9a9c5a785d361b245c4b4
Frederic Hemberger <mail@frederic-hemberger.de>
2014-05-18 14:47:35 +0200

    Add HTML slides of code organization talk

---
+++ js-codeorganisation/slide_config.js
@@ -0,0 +1,23 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Code-Organisation in JavaScript',
+    subtitle: 'Webworker-Stammtisch NRW<br>10. Januar 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Frederic Hemberger',
+    twitter: '@fhemberger',
+    www: 'http://frederic-hemberger.de'
+  }]
+};
+
